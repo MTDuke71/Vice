25c58f9e7e09557a7ba303cab5ae7d389b66aceb
MTDuke71 <matt.laduke@gmail.com>
2023-04-21 22:47:14 -0400

    part37

--- vice.c
+++ vice.c
@@ -4,20 +4,17 @@
 #include "defs.h"
 #include "stdlib.h"
 
-#define PAWNMOVESW "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
-#define PAWNMOVESB "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1"
+#define CASTLE1 "r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1"
+#define CASTLE2 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
 
 int main() {	
 
 	AllInit();		
 	
 	S_BOARD board[1];
-	
-	ParseFen(PAWNMOVESB,board);
-	PrintBoard(board);
-	
 	S_MOVELIST list[1];
 	
+	ParseFen(CASTLE2,board);
 	GenerateAllMoves(board,list);
 	
 	PrintMoveList(list);
